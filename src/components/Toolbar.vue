4001610451a80445767d60ec512ed1c81fea1178
Emilia <eboghiu@lemonway.com>
2020-07-10 15:39:32 +0200

    first commit with fake data

---
+++ src/components/Toolbar.vue
@@ -0,0 +1,72 @@
+<template>
+<div>
+       <!-- <v-app-bar
+      app
+      color="white"
+    > -->
+    
+       <!-- <v-avatar
+        class="mr-3"
+        color="grey lighten-5"
+        size="60"
+      >
+        <v-img
+          contain
+          max-height="70%"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+        ></v-img>
+      </v-avatar>   -->
+      <v-app-bar>
+   <v-app-bar-nav-icon class="hidden-sm-and-up" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+       <v-toolbar-title>
+                <router-link class="nav-item" to="/"> <img src="\assets\AdiLogo.png"></router-link>
+            </v-toolbar-title>
+      <v-spacer></v-spacer>
+       <v-toolbar-items class="hidden-sm-and-down">
+          <v-list-item  v-for="item in links" :key="item.text" router :to="item.route">
+        
+            <v-list-item-title>{{item.text}}</v-list-item-title>
+            <!-- <v-list-item-action>
+              <v-icon >{{item.icon}}</v-icon>
+            </v-list-item-action> -->
+        </v-list-item>
+       
+     
+       </v-toolbar-items>
+      </v-app-bar>
+      <v-list dense v-if="drawer">
+          <v-list-item-group   v-for="(item, i) in links"
+          :key="i" color="primary">
+            <v-list-item @click.stop="drawer = !drawer" router :to="item.route"
+        
+        >  <v-list-item-content>
+            <v-list-item-title  v-text="item.text"></v-list-item-title>
+          </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+      </v-list>
+    <!-- </v-app-bar>   -->
+   
+   
+</div>
+
+</template>
+<script>
+export default {
+    data(){
+        return{
+             links: [
+          {text:'Accueil',route: '/'},
+          { text:'A propos de moi',route: '/abouts'},
+          { text:'Realisations',route: '/realisations'},
+          { text:'Devis',route: '/devis'},
+          { icon:'email',text:"Contact",route: '/contact'} 
+      ],
+      drawer:null
+        }
+    }
+}
+    
+
+</script>
---
+++ src/views/Devis.vue
@@ -0,0 +1,131 @@
+<template>
+    <div id="contact">
+       <v-app>
+   
+    <v-content>
+        <v-sheet
+        id="contact"
+        color="#333333"
+        dark
+        tag="section"
+        tile
+      >
+        <div class="py-12"></div>
+
+        <v-container>
+          <h2 class="display-2 font-weight-bold mb-3  text-center">Demande de devis</h2>
+
+          <v-responsive
+            class="mx-auto mb-12"
+            width="56"
+          >
+            <v-divider class="mb-1"></v-divider>
+
+            <v-divider></v-divider>
+          </v-responsive>
+       
+           <div><strong>Art Menuiserie</strong> s'engage à être réactif. Demandez-nous un devis pour tout type de réalisation en bois sur mesure et nous vous répondrons dans les meilleurs délais, en fonction de votre demande !</div>
+        
+        <br>
+          <v-theme-provider light>
+            <v-row>
+                
+                <v-col cols="12">
+                <v-text-field
+                  flat
+                  label="Nom*"
+                  solo
+                ></v-text-field>
+              </v-col>
+              <v-col cols="6">
+                <v-text-field
+                  flat
+                  label="Téléphone*"
+                  solo
+                ></v-text-field>
+              </v-col>
+
+              <v-col cols="6">
+                <v-text-field
+                  flat
+                  label="Email*"
+                  solo
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field
+                  flat
+                  label="Adresse*"
+                  solo
+                ></v-text-field>
+              </v-col>
+               <v-col cols="6">
+                <v-text-field
+                  flat
+                  label="Ville*"
+                  solo
+                ></v-text-field>
+              </v-col>
+
+              <v-col cols="6">
+                <v-text-field
+                  flat
+                  label="Code postal*"
+                  solo
+                ></v-text-field>
+              </v-col>
+
+              <v-col cols="12">
+                <v-text-field
+                  flat
+                  label="Sujet*"
+                  solo
+                ></v-text-field>
+              </v-col>
+
+              <v-col cols="12">
+                <v-textarea
+                  flat
+                  label="Message*"
+                  solo
+                ></v-textarea>
+              </v-col>
+              <v-col cols="6">
+                <v-file-input
+                  label="File input"
+                  filled
+                  background-color="white"
+                  prepend-icon="mdi-camera"
+                ></v-file-input>
+              </v-col>
+               <v-col cols="6">
+                <v-file-input
+                  label="File input"
+                  filled
+                  background-color="white"
+                  prepend-icon="mdi-camera"
+                 text-color="white"
+                ></v-file-input>
+              </v-col>
+
+              <v-col
+                class="mx-auto"
+                cols="auto"
+              >
+                <v-btn
+                  color="accent"
+                  x-large
+                >
+                  Envoyer
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-theme-provider>
+        </v-container>
+
+        <div class="py-12"></div>
+      </v-sheet>
+    </v-content>
+    </v-app>
+    </div>
+</template>
